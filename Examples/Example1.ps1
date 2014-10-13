4f9ab021e9f399240c26a16f41d6cfa1fb209418
Justin-DynamicD <justin.king@prosum.com>
2014-10-13 15:10:57 -0700

    cFailoverCluster
    DSC resource to manage cluster networking components

---
+++ Examples/Example1.ps1
@@ -0,0 +1,66 @@
+﻿Configuration HyperVCluster
+{ 
+    Import-DSCResource -ModuleName cFailoverCluster
+    
+    Node $AllNodes.NodeName
+    { 
+        cClusterNetworking PublicClusterNetwork
+        { 
+            NetworkSubnet      = $Node.ClusterNetworkPublicSubnet
+            NetworkPrefix = $Node.ClusterNetworkPublicPrefix
+            NetworkName     = $Node.ClusterNetworkPublicName
+        }
+
+        cClusterNetworking CSVClusterNetwork
+        { 
+            NetworkSubnet      = $Node.ClusterNetworkCSVSubnet
+            NetworkPrefix = $Node.ClusterNetworkCSVPrefix
+            NetworkName     = $Node.ClusterNetworkCSVName
+            NetworkMetric  = $Node.ClusterNetworkCSVMetric
+        }
+
+        cClusterNetworking LMClusterNetwork
+        { 
+            NetworkSubnet      = $Node.ClusterNetworkLMSubnet
+            NetworkPrefix = $Node.ClusterNetworkLMPrefix
+            NetworkName     = $Node.ClusterNetworkLMName
+            NetworkMetric  = $Node.ClusterNetworkLMMetric
+        }
+    }
+}
+
+$ClusterConfig = @{ 
+    AllNodes = @( 
+ 
+        @{ 
+            NodeName= "*" 
+            ClusterNetworkPublicSubnet = "10.8.115.0"
+            ClusterNetworkPublicPrefix = "24"
+            ClusterNetworkPublicName = "Public"
+            ClusterNetworkCSVSubnet = "10.11.101.0"
+            ClusterNetworkCSVPrefix = "24"
+            ClusterNetworkCSVName = "Cluster Shared Volume"
+            ClusterNetworkCSVMetric = "800"
+            ClusterNetworkLMSubnet = "10.11.100.0"
+            ClusterNetworkLMPrefix = "24"
+            ClusterNetworkLMName = "Live Migration"
+            ClusterNetworkLMMetric = "1000"
+        }, 
+ 
+         # Node01 
+        @{ 
+            NodeName= "Server01"
+         }, 
+ 
+         # Node02 
+         @{ 
+            NodeName= "Server02"
+         },
+        
+         # Node03 
+         @{ 
+            NodeName= "Server03"
+         }
+    ); 
+} 
+
